7283b7817ec2487fc4e720e4dfd819c321e0c9df
Cesc Blanco <cesc.blanco98@gmail.com>
2026-07-12 14:17:53 +0200

    feat: supabase

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+from database import supabase
+
 
 st.title("Resultados de las 5 grandes ligas")
 
 st.write("Mi aplicación de fútbol")
+
+respuesta = supabase.table("usuarios").select("*").execute()
+
+st.write(respuesta.data)
